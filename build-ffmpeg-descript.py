b948a2def3e9956d9370267c060f94dc5f1e5ef8
C Van Winkle <accounts+github@cvanwinkle.com>
2021-11-09 11:46:18 -0600

    more cleanup from node script

--- build-ffmpeg-descript.py
+++ build-ffmpeg-descript.py
@@ -88,108 +88,6 @@ def copyOrGenerateSymbolFiles(source, dest):
     for fileref in pathlib.Path(source + '/').glob('**/*.dylib'):
         copyOrGenerateSymbolFile(str(fileref), dest)
 
-'''
-const { resolve, basename } = require('path');
-const { mkdirSync, readlinkSync, readdirSync } = require('fs');
-const { execSync } = require('child_process');
-
-function logAndExec(cmd) {
-  console.log(`EXEC ${cmd}`);
-  execSync(cmd);
-}
-
-const baseIncludesDir = resolve(__dirname, 'workspace/include');
-const baseBinDir = resolve(__dirname, 'workspace/bin');
-const baseLibDir = resolve(__dirname, 'workspace/lib');
-const destDir = resolve(__dirname, 'workspace/mac');
-
-try {
-  logAndExec(`rm -r -f ${destDir}`);
-} catch (err) {
-  //
-}
-logAndExec(`mkdir -p ${destDir}`);
-
-const skippedLibs = new Set();
-const copiedLibs = new Set();
-const missingLibs = new Set();
-const namesWithoutVersion = new Set();
-
-function copyDylibs(binaryName, base = baseBinDir) {
-  const origPath = resolve(base, binaryName);
-  const binaryPath = resolve(destDir, binaryName);
-
-  logAndExec(`cp -a ${origPath} ${binaryPath}`);
-
-  const lines = execSync(`otool -L ${binaryPath}`).toString('utf8').split('\n');
-  const libsToRewrite = [];
-  for (const line of lines) {
-    const match = /[^\s:]+/.exec(line);
-    if (!match) {
-      continue;
-    }
-    const [path] = match;
-    if (path.startsWith('/usr/local')) {
-      missingLibs.add(path);
-    } else if (path.startsWith('/Users')) {
-      const filename = basename(path);
-      const newFilename = resolve(destDir, filename);
-      if (!copiedLibs.has(path)) {
-        copiedLibs.add(path);
-        copiedLibs.add(newFilename);
-        if (path !== newFilename) {
-          // copy sym-linked libraries as well
-          let nameWithoutVersion = filename.split('.')[0];
-          // libSDL2 weirdly has hypthen after then name (i.e., libSDL2-2.0.0.dylib)
-          if (filename.includes('libSDL2')) {
-            nameWithoutVersion = 'libSDL2';
-          }
-          namesWithoutVersion.add(nameWithoutVersion);
-          const nameWithoutVersionLib = `${nameWithoutVersion}.dylib`;
-          logAndExec(`cp -a ${resolve(baseLibDir, nameWithoutVersion)}*.dylib ${destDir}/.`);
-
-          copyDylibs(filename, baseLibDir);
-        }
-      }
-      libsToRewrite.push({path, filename});
-    } else {
-      skippedLibs.add(path);
-    }
-  }
-
-  // find the non-sym-linked version of this library
-  let actualBinaryPath = binaryPath;
-  try {
-    const actualBinaryName = readlinkSync(binaryPath);
-    actualBinaryPath = resolve(destDir, actualBinaryName);
-  } catch (err) {
-    //
-  }
-
-  if (libsToRewrite.length > 0) {
-    logAndExec(`install_name_tool -id @loader_path/${binaryName} ${libsToRewrite.map(({path, filename}) => `-change ${path} @loader_path/${filename}`).join(' ')} ${actualBinaryPath}`);
-  }
-}
-
-copyDylibs('ffmpeg');
-copyDylibs('ffprobe');
-
-console.log('Copying includes');
-logAndExec(`cp -r ${baseIncludesDir} ${destDir}/.`);
-
-for (const lib of Array.from(skippedLibs).sort()) {
-  console.log(`[NOTE] skipped ${lib}`);
-}
-for (const lib of Array.from(copiedLibs).sort()) {
-  if (!lib.startsWith(destDir)) {
-    console.log(`Copied ${lib}`);
-  }
-}
-for (const lib of Array.from(missingLibs).sort()) {
-  console.log(`[WARNING] missing ${lib}`);
-}
-'''
-
 #
 #
 #
@@ -244,6 +142,8 @@ def copyLibraryAndDependencies(src_file, dest_folder):
         if not match:
             continue
         src_dependency_file = match[0]
+        if (src_dependency_file == 'libvpx.so.6'):
+          breakpoint;
         if src_dependency_file.startswith('/usr/local'):
             missing_libs.add(src_dependency_file)
         elif src_dependency_file.startswith(workspace_dir):
@@ -292,7 +192,7 @@ def main():
     #buildFFmpeg(cwd, workspace_dir)
     
     # Generate dSYM files for each built library
-    #copyOrGenerateSymbolFiles(packages_dir, workspace_lib_dir)
+    copyOrGenerateSymbolFiles(packages_dir, workspace_lib_dir)
 
     # Generate dSYM files for each executable
     executables = ['ffmpeg', 'ffprobe']
@@ -300,6 +200,20 @@ def main():
         executable_path = os.path.join(workspace_bin_dir, executable)
         copyOrGenerateSymbolFile(executable_path, workspace_bin_dir)
         copyLibraryAndDependencies(executable_path, output_dir)
+
+    # Copy Includes
+    shutil.copytree(
+      os.path.join(workspace_dir, 'include'),
+      os.path.join(output_dir, 'include'))
+
+    for lib in sorted(skipped_libs):
+      print ('[NOTE] skipped ' + lib)
+
+    for lib in sorted(copied_libs):
+      print ('Copied ' + lib)
+
+    for lib in sorted(missing_libs):
+      print ('[WARNING] missing ' + lib)
 
 #
 #   entry
